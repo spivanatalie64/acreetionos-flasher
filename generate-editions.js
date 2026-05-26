572db49e96354c99f5860aebe7167256fae8fb8b
Natalie Spiva <spivajohnathan64@gmail.com>
2026-05-26 13:47:12 -0700

    Fix CI: add weekly schedule, robust release creation. Fix generate-editions: add all R2 fallbacks, support more repos. Fix editions: add MATE/GNOME ISO URLs.

--- generate-editions.js
+++ generate-editions.js
@@ -9,7 +9,20 @@ const path = require('path');
 const EDITIONS_FILE = path.join(__dirname, 'src-tauri', 'editions.json');
 const WEB_EDITIONS_FILE = path.join(__dirname, 'src', 'editions.json');
 const GITHUB_API = 'https://api.github.com';
-const CF_R2_PUBLIC = 'https://pub-173a1f638a3b4c95b5f58b09c0b968aa.r2.dev';
+const CF_R2 = 'https://pub-173a1f638a3b4c95b5f58b09c0b968aa.r2.dev';
+
+const DEFAULT_ISO_URLS = {
+  'cinnamon': 'https://iso.acreetionos.org:8448/acreetion/AcreetionOS-1.0-x86_64.iso',
+  'xl': 'https://iso.acreetionos.org:8448/acreetion/AcreetionOS_XL-1.0-x86_64.iso',
+  '32bit': `${CF_R2}/AcreetionOS32-latest.iso`,
+  'hyprland': `${CF_R2}/AcreetionOS-Hyprland-latest.iso`,
+  'plasma': `${CF_R2}/AcreetionOS-Plasma-latest.iso`,
+  'mate': `${CF_R2}/AcreetionOS-MATE-latest.iso`,
+  'gnome': `${CF_R2}/AcreetionOS-GNOME-latest.iso`,
+  'xfce': `${CF_R2}/AcreetionOS-XFCE-latest.iso`,
+  'sway': `${CF_R2}/AcreetionOS-Sway-latest.iso`,
+  'i3': `${CF_R2}/AcreetionOS-i3-latest.iso`,
+};
 
 async function get(url) {
   return new Promise((resolve, reject) => {
@@ -36,12 +49,29 @@ async function getLatestRelease(repo) {
 }
 
 async function main() {
-  const editions = JSON.parse(fs.readFileSync(EDITIONS_FILE, 'utf-8'));
+  let editions;
+  try {
+    editions = JSON.parse(fs.readFileSync(EDITIONS_FILE, 'utf-8'));
+  } catch {
+    editions = [];
+  }
 
-  // For editions that have GitHub releases, fetch latest
+  // Apply default R2 URLs for any edition that's missing one
+  for (const ed of editions) {
+    if (!ed.iso_url && DEFAULT_ISO_URLS[ed.name]) {
+      ed.iso_url = DEFAULT_ISO_URLS[ed.name];
+    }
+    if (!ed.zip_url && DEFAULT_ISO_URLS[ed.name]) {
+      ed.zip_url = DEFAULT_ISO_URLS[ed.name].replace('.iso', '.iso.zip');
+    }
+  }
+
+  // Try to fetch latest ISOs from GitHub releases for editions that have repos
   const releaseRepos = {
     'mate': 'spivanatalie64/AcreetionOS-Mate',
     'gnome': 'spivanatalie64/acreetionos-gnome',
+    'hyprland': 'spivanatalie64/acreetionos-hyprland',
+    '32bit': 'spivanatalie64/acreetionos32',
   };
 
   for (const [name, repo] of Object.entries(releaseRepos)) {
